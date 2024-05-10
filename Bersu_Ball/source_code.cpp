5047f12caad8629a966a53fa035bd8069fdad53c
vedica mishra <100271946+vediicaa@users.noreply.github.com>
2024-05-10 10:44:03 +0530

    Create source_code.cpp

---
+++ Bersu_Ball/source_code.cpp
@@ -0,0 +1,39 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n;
+    vector<int> boys(n);
+    for (int i = 0; i < n; i++) {
+        cin >> boys[i];
+    }
+    cin >> m;
+    vector<int> girls(m);
+    for (int i = 0; i < m; i++) {
+        cin >> girls[i];
+    }
+
+    sort(boys.begin(), boys.end());
+    sort(girls.begin(), girls.end());
+
+    int pairs = 0;
+    int i = 0, j = 0;
+    while (i < n && j < m) {
+        if (abs(boys[i] - girls[j]) <= 1) {
+            pairs++;
+            i++;
+            j++;
+        } else if (boys[i] < girls[j]) {
+            i++;
+        } else {
+            j++;
+        }
+    }
+
+    cout << pairs << endl;
+
+    return 0;
+}
