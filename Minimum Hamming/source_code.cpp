02e1bce3f89b9f72f1ab6e09abc862f714b9458c
vedica mishra <100271946+vediicaa@users.noreply.github.com>
2024-05-10 10:50:33 +0530

    Update source_code.cpp

--- Minimum Hamming/source_code.cpp
+++ Minimum Hamming/source_code.cpp
@@ -1,1 +1,35 @@
 
+#include <iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int totalHammingDistance(vector<int>& nums) {
+    int hammingdistance=0;
+    for(int bitpos=0;bitpos<32;bitpos++){
+    int setbits=0 ;
+        for(int i = 0 ;i<nums.size();i++){
+            setbits+=(nums[i]>>bitpos)&1;
+           
+        }
+        hammingdistance+=setbits * (nums.size()-setbits);
+    }
+    return hammingdistance;
+
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int>v;
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        v.push_back(x);
+    }
+    int ans=totalHammingDistance(v);
+    
+    cout<<ans;
+}
